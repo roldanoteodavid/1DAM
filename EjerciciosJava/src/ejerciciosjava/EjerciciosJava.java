b563e825c76451084623666a8b8a78694cccc6e3
david <david.roldanoteo@educa.madrid.org>
2022-12-09 14:12:14 +0100

    Primer cambio desde  Eclipse.

--- EjerciciosJava/src/ejerciciosjava/EjerciciosJava.java
+++ EjerciciosJava/src/ejerciciosjava/EjerciciosJava.java
@@ -16,5 +16,6 @@ public class EjerciciosJava {
     public static void main(String[] args) {
         System.out.println("Primer cambio desde NetBeans.");
         System.out.println("Primer cambio desde IntelliJ.");
+        System.out.println("Primer cambio desde Eclipse.");
     }
 }
